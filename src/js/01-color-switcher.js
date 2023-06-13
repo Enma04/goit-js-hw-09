8c072acf966c65434e5f26dbddeef98f80b75216
Enma04 <programmer1ebg@gmail.com>
2023-06-13 17:27:11 -0500

    Punto 1 sin el 'disabled'

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+//-------------------------------------------------------
+//-------------------- VARIABLES ------------------------
+const body = document.querySelector("body");
+const btnStart = document.querySelector("[data-start]");
+const btnStop = document.querySelector("[data-stop]");
 
+let bodyColor = null;
+
+
+//-------------------------------------------------------
+//-------------------- FUNCTIONS ------------------------
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+
+
+//-------------------------------------------------------
+//-------------------- IMPLEMENTS -----------------------
+btnStart.addEventListener("click", () => {
+	bodyColor = setInterval(() => {
+		body.style.backgroundColor = getRandomHexColor();
+	}, 1000);
+});
+
+btnStop.addEventListener("click", () => {
+	clearInterval(bodyColor);
+  console.log(`Interval with id ${timerId} has stopped!`);
+});

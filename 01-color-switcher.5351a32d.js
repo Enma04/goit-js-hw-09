836bba92d926f5ab30119f408c9188078b679edb
Enma04 <118761090+Enma04@users.noreply.github.com>
2023-06-13 22:41:09 +0000

    Deploying to gh-pages from @ Enma04/goit-js-hw-09@2a47a01987dcb7f5023c74203c548b65668054c4 🚀

--- 01-color-switcher.5351a32d.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("body"),e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),o=null;e.addEventListener("click",(function(){o=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),n.addEventListener("click",(function(){clearInterval(o),console.log("Interval with id ".concat(timerId," has stopped!"))}))}();
-//# sourceMappingURL=01-color-switcher.5351a32d.js.map

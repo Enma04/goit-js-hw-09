5d12733304398bc6d9b0dcae9dea5ee74458d8f1
Enma04 <programmer1ebg@gmail.com>
2023-06-14 10:31:39 -0500

    Punto 3, v1.0

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,50 @@
-function createPromise(position, delay) {
+import Notiflix from 'notiflix';
+
+/*
+Notiflix simple messages:
+-------------------------------
+
+Notiflix.Notify.success('Sol lucet omnibus');
+
+Notiflix.Notify.failure('Qui timide rogat docet negare');
+
+Notiflix.Notify.warning('Memento te hominem esse');
+
+Notiflix.Notify.info('Cogito ergo sum');
+*/
+
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+const btnStart = document.querySelector("button");
+
+
+function createPromise(position, step) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve("success!");
+      } else {
+        reject("error!");
+      }
+    }, step); //setTimeout
+  }); //promise
+} //function
+
+
+btnStart.addEventListener("click", () => {
+  console.log("delay: ", delay);
+  console.log("step: ", step);
+  console.log("amount: ", amount);
+
+  setTimeout(() => {
+    for(let i=1; i<=amount; i+=1) {
+      createPromise(i, step)
+      .then(({position, delay}) => console.log(`✅ Fulfilled promise ${position} in ${delay}ms`))
+      .catch(({position, delay}) => console.log(`❌ Rejected promise ${position} in ${delay}ms`));
+    }
+  }, delay);
+});
+
+
